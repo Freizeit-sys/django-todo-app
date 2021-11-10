2aeb41e3fe9ce4405e668765755e03e6d76c0859
Freizeit-sys <work16087y0rha@gmail.com>
2021-11-10 17:30:04 +0900

    create todo_update.html

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.TodoListView.as_view(), name='todo_list'),
     path('todo_detail/<int:pk>', views.TodoDetailView.as_view(), name='todo_detail'),
+    path('todo_create', views.TodoCreateView.as_view(), name='todo_create'),
+    path('todo_update', views.TodoUpdateView.as_view(), name='todo_update'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,7 @@
+from django.db.models import fields
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
 
 from .models import Todo
 
@@ -10,3 +12,13 @@ class TodoListView(ListView):
 class TodoDetailView(DetailView):
     model = Todo
     context_object_name = 'task'
+    
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = '__all__'
+    success_url = reverse_lazy('todo_list')
+    
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = '__all__'
+    success_url = reverse_lazy('todo_list')
