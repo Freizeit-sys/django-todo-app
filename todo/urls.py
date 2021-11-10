79164141bcd9e11a60fcda34c34b072bf7f2f9de
Freizeit-sys <work16087y0rha@gmail.com>
2021-11-10 17:58:41 +0900

    create todo_confirm_delete.html

--- todo/urls.py
+++ todo/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.TodoListView.as_view(), name='todo_list'),
     path('todo_detail/<int:pk>', views.TodoDetailView.as_view(), name='todo_detail'),
     path('todo_create', views.TodoCreateView.as_view(), name='todo_create'),
-    path('todo_update', views.TodoUpdateView.as_view(), name='todo_update'),
+    path('todo_update/<int:pk>', views.TodoUpdateView.as_view(), name='todo_update'),
+    path('todo_delete/<int:pk>', views.TodoDeleteView.as_view(), name='todo_delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,12 @@
-from django.db.models import fields
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Todo
 
 class TodoListView(ListView):
     model = Todo
-    content_object_name = 'tasks'
+    context_object_name = 'tasks'
     
 class TodoDetailView(DetailView):
     model = Todo
@@ -21,4 +20,9 @@ class TodoCreateView(CreateView):
 class TodoUpdateView(UpdateView):
     model = Todo
     fields = '__all__'
+    success_url = reverse_lazy('todo_list')
+    
+class TodoDeleteView(DeleteView):
+    model = Todo
+    context_object_name = 'task'
     success_url = reverse_lazy('todo_list')
